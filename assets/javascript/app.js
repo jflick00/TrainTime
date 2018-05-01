7750bdebef2395c80f28348ef4a63b4674c2e2ff
jflick00 <jflick2389@gmail.com>
2018-05-01 15:18:22 -0400

    Added background image. Added more styling in CSS.  Updated JavaScript.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,28 +1,35 @@
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyCsZ9J0MPY5XA_PW_kkpKFuH71Tdgg1hHs",
-    authDomain: "train-time-940a7.firebaseapp.com",
-    databaseURL: "https://train-time-940a7.firebaseio.com",
-    projectId: "train-time-940a7",
-    storageBucket: "train-time-940a7.appspot.com",
-    messagingSenderId: "710285382112"
-  };
+    apiKey: "AIzaSyAiN03J8efV7X76-ts7ziYhaIJu8-mJmjk",
+    authDomain: "traintime-c7988.firebaseapp.com",
+    databaseURL: "https://traintime-c7988.firebaseio.com",
+    projectId: "traintime-c7988",
+    storageBucket: "",
+    messagingSenderId: "564207673925"
+};
   
 firebase.initializeApp(config);
 
 var database = firebase.database();
 
+
+//  Displays todays date and time
+var now = moment().format("MM/DD/YYYY hh:mm A");
+$('#currentTime').text("Today is: " + now);
+
 //  Click function for adding Train
 $("#add-train").on("click", function(event) {
-    //  Keeps for from submitting on click
+
+    //  Keeps form from submitting on click
     event.preventDefault();
-console.log("hey");
+
     // Grabs user input
     var name = $("#train_name").val().trim();
     var destination = $("#destination").val().trim();
     var time = moment($("#train_time").val().trim(), "HH:mm").format("X");
     var frequency = $("#frequency").val().trim();
 
+    //  Creating temporary object
     var newTrain = {
         newName: name,
         newDestination: destination,
@@ -46,11 +53,27 @@ database.ref().on("child_added", function(childSnapshot, prevChildKey) {
     var destination = childSnapshot.val().newDestination;
     var time = childSnapshot.val().newTime;
     var frequency = childSnapshot.val().newFrequency;
+    
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(time, "HH:mm").subtract(1, "years");
 
-    // var timeDisplay = moment.unix(time).format("HH:mm");
+    // Current Time
+    var currentTime = moment();
 
-    // var tMinutes = moment().diff(moment(empStart, "X"), "months");
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
 
+    // Time apart (remainder)
+    var tRemainder = diffTime % frequency;
+
+    // Minute Until Train
+    var minutesAway = frequency - tRemainder;
+
+    // Next Train
+    var nextArrival = moment().add(minutesAway, "minutes");
+    nextArrival = moment(nextArrival).format("hh:mm A");
+    
+    // Adding to table
     $("#train-table > tbody").append("<tr><td>" + name + "</td><td>" + destination + "</td><td>" +
-    time + "</td><td>" + frequency + "</td><td>");
+    frequency + "</td><td>" + nextArrival + "</td><td>" + minutesAway + "</td></tr>");
 });
